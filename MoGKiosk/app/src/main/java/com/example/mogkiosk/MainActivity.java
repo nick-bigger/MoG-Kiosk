242dfed1bb2b49e6db678b1222fbec91778f04f3
aysehunt <aysehunt@gmail.com>
2019-02-21 20:41:23 -0800

    adding fragments and image to home page

--- MoGKiosk/app/src/main/java/com/example/mogkiosk/MainActivity.java
+++ MoGKiosk/app/src/main/java/com/example/mogkiosk/MainActivity.java
@@ -109,9 +109,21 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            Fragment fragment = null;
+
+            switch(position){
+                case 0:
+                    fragment = new ArtistFrag();
+                    break;
+                case 1:
+                    fragment = new WorkFrag();
+                    break;
+                case 2:
+                    fragment = new ProcessFrag();
+                    break;
+
+            }
+            return fragment;
         }
 
         @Override
