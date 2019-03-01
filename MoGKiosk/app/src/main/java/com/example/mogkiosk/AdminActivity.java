f1ce23284374499eb6dd0d706e330812c0305305
mjohnstone14 <mjohnstone@eaglerockschool.org>
2019-02-28 20:48:53 -0800

    added layout files for all three tabs, set up preliminary connections, still need to pass data to front end

--- MoGKiosk/app/src/main/java/com/example/mogkiosk/AdminActivity.java
+++ MoGKiosk/app/src/main/java/com/example/mogkiosk/AdminActivity.java
@@ -34,7 +34,6 @@ public class AdminActivity extends AppCompatActivity {
         setContentView(R.layout.admin_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setLogo(R.mipmap.ic_launcher_round);
         //setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
