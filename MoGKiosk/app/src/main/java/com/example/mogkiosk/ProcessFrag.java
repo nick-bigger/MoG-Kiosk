4dd0fe1629ed78ccf11b206ab903139e9416a662
nbigger <nick.bigger@gmail.com>
2019-03-01 21:29:34 -0800

    added process tab basic ui with video functionality

--- MoGKiosk/app/src/main/java/com/example/mogkiosk/ProcessFrag.java
+++ MoGKiosk/app/src/main/java/com/example/mogkiosk/ProcessFrag.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 
 /**
@@ -63,8 +65,21 @@ public class ProcessFrag extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_process, container, false);
+        VideoView videoView = (VideoView) rootView.findViewById(R.id.videoView);
+        String videoPath = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(getActivity());
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+        videoView.start();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_process, container, false);
+        return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
