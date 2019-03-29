fa3095e9bd9a65d1d674342f532edfb0aa9a35bf
nbigger <nick.bigger@gmail.com>
2019-03-29 12:13:44 -0700

    ui tweaks to make application reflect web design better

--- MoGKiosk/app/src/main/java/com/example/mogkiosk/MainActivity.java
+++ MoGKiosk/app/src/main/java/com/example/mogkiosk/MainActivity.java
@@ -1,24 +1,21 @@
 package com.example.mogkiosk;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-   
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-  
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -61,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
       fab.setOnClickListener(new View.OnClickListener() {
           @Override
           public void onClick(View view) {
-              Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+              Snackbar.make(view, "Feedback sent!", Snackbar.LENGTH_LONG)
                       .setAction("Action", null).show();
           }
       });
