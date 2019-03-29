4eafe8ed9a31da43faefdf8e2c0caaeb4babd1e9
nbigger <nick.bigger@gmail.com>
2019-03-29 14:14:57 -0700

    removed fab

--- MoGKiosk/app/src/main/java/com/example/mogkiosk/MainActivity.java
+++ MoGKiosk/app/src/main/java/com/example/mogkiosk/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.mogkiosk;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -53,15 +51,6 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
-
-      FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-      fab.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              Snackbar.make(view, "Feedback sent!", Snackbar.LENGTH_LONG)
-                      .setAction("Action", null).show();
-          }
-      });
       
     }
 
