2cd9a81a593b5b65b3105484c80884dd78377f1f
nbigger <nick.bigger@gmail.com>
2019-04-19 14:29:03 -0700

    added super secret way to get to login page

--- MoGKiosk/app/src/main/java/com/example/mogkiosk/activities/AboutActivity.java
+++ MoGKiosk/app/src/main/java/com/example/mogkiosk/activities/AboutActivity.java
@@ -1,12 +1,17 @@
 package com.example.mogkiosk.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.example.mogkiosk.LoginActivity;
 import com.example.mogkiosk.R;
 
 public class AboutActivity extends AppCompatActivity {
+    static int numClicks = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,17 @@ public class AboutActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ImageView img = (ImageView) findViewById(R.id.dev_photo_3);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numClicks++;
+                if (numClicks > 9) {
+                    startActivity(new Intent(AboutActivity.this, LoginActivity.class));
+                }
+            }
+        });
     }
 
 }
