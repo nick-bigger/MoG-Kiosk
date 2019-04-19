358c4e2efff957f626cf52190068d1ac83ec899c
aysehunt <aysehunt@gmail.com>
2019-04-18 22:47:48 -0700

    created layouts for change password and update credentials

---
+++ MoGKiosk/app/src/main/java/com/example/mogkiosk/UpdateCredActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.mogkiosk;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class UpdateCredActivityFragment extends Fragment {
+
+    public UpdateCredActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_update_cred, container, false);
+    }
+}
