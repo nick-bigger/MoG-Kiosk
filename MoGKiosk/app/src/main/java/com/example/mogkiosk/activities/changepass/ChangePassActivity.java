ebd6ba75c46382f264003694ee7615f786728970
nbigger <nick.bigger@gmail.com>
2019-05-02 18:41:48 -0700

    updated design for change password page input text boxes

--- MoGKiosk/app/src/main/java/com/example/mogkiosk/activities/changepass/ChangePassActivity.java
+++ MoGKiosk/app/src/main/java/com/example/mogkiosk/activities/changepass/ChangePassActivity.java
@@ -2,13 +2,14 @@
 package com.example.mogkiosk.activities.changepass;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.example.mogkiosk.PrivateInfoManager;
 import com.example.mogkiosk.R;
@@ -22,9 +23,11 @@ public class ChangePassActivity extends AppCompatActivity {
     private EditText mRetypePassTextView;
     private EditText mNewUserTextView;
 
-    private TextView mNotLongEnoughPassTextView;
-    private TextView mNotLongEnoughUserTextView;
-    private TextView mPassDontMatchTextView;
+    private TextInputLayout usrInput;
+    private TextInputLayout passInput;
+    private TextInputLayout rePassInput;
+
+    private Drawable lockIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,73 +43,60 @@ public class ChangePassActivity extends AppCompatActivity {
         mNewPassTextView = findViewById(R.id.password);
         mRetypePassTextView = findViewById(R.id.retype_password);
         mNewUserTextView = findViewById(R.id.username);
+
+        usrInput = findViewById(R.id.usernameInputLayout);
+        passInput = findViewById(R.id.passInputLayout);
+        rePassInput = findViewById(R.id.retypePassInputLayout);
+
         Button mSubmitChanges = findViewById(R.id.submit);
-
-        mNotLongEnoughPassTextView = findViewById(R.id.pass_length);
-        mNotLongEnoughUserTextView = findViewById(R.id.username_length);
-        mPassDontMatchTextView = findViewById(R.id.do_not_match);
-
-        mNotLongEnoughPassTextView.setVisibility(View.INVISIBLE);
-        mNotLongEnoughUserTextView.setVisibility(View.INVISIBLE);
-        mPassDontMatchTextView.setVisibility(View.INVISIBLE);
 
         mSubmitChanges.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
-                mNotLongEnoughPassTextView.setVisibility(View.INVISIBLE);
-                mNotLongEnoughUserTextView.setVisibility(View.INVISIBLE);
-                mPassDontMatchTextView.setVisibility(View.INVISIBLE);
 
                 boolean userLength = mNewUserTextView.getText().toString().length() < 5;
                 boolean passLength = mNewPassTextView.getText().toString().length() < 5;
                 boolean retypeGood = mRetypePassTextView.getText().toString().equals(mNewPassTextView.getText().toString());
 
-                if(userLength && passLength && !retypeGood)
-                {
-                    mNotLongEnoughUserTextView.setVisibility(View.VISIBLE);
-                    mNotLongEnoughPassTextView.setVisibility(View.VISIBLE);
-                    mPassDontMatchTextView.setVisibility(View.VISIBLE);
-                    System.out.println("Not Long enough user and password and does not match");
-                }
-                else if (userLength && passLength)
-                {
-                    mNotLongEnoughUserTextView.setVisibility(View.VISIBLE);
-                    mNotLongEnoughPassTextView.setVisibility(View.VISIBLE);
-                    System.out.println("Not Long enough username and pass");
-                }
-                else if(userLength && !retypeGood)
-                {
-                    mNotLongEnoughUserTextView.setVisibility(View.VISIBLE);
-                    mPassDontMatchTextView.setVisibility(View.VISIBLE);
-                    System.out.println("Not Long enough username and pass dont match");
-                }
-                else if(passLength && ! retypeGood)
-                {
-                    mNotLongEnoughPassTextView.setVisibility(View.VISIBLE);
-                    mPassDontMatchTextView.setVisibility(View.VISIBLE);
-                    System.out.println("Not Long enough password and does not match");
+                if (userLength) {
+                    usrInput.setError(getString(R.string.error_usn));
+                    usrInput.setHint("Invalid Username");
 
-                }
-                else if (userLength)
-                {
                     //Do a you need at least 5 character message
-                    mNotLongEnoughUserTextView.setVisibility(View.VISIBLE);
                     System.out.println("Not Long enough username");
+                } else {
+                    usrInput.setError(null);
+                    usrInput.setHint("New Username");
                 }
-                else if (passLength)
-                {
+
+                if (passLength) {
+                    passInput.setHint("Invalid Password");
+                    passInput.setError(getString(R.string.error_pwd));
+
                     //Do a you need at least 5 character message
-                    mNotLongEnoughPassTextView.setVisibility(View.VISIBLE);
                     System.out.println("Not Long enough password");
+                } else {
+                    passInput.setError(null);
+                    passInput.setHint("New Password");
                 }
-                else if (! retypeGood)
-                {
+
+                if (!retypeGood) {
+                    rePassInput.setHint("Retype Mismatch");
+                    rePassInput.setError("Passwords do not match.");
+
                     //Passwords do not match
-                    mPassDontMatchTextView.setVisibility(View.VISIBLE);
                     System.out.println("Do not match");
+                } else {
+                    rePassInput.setError(null);
+                    rePassInput.setHint("Retype New Password");
                 }
-                else {
+
+                if (!userLength && !passLength && retypeGood) {
+                    usrInput.setErrorEnabled(false);
+                    passInput.setErrorEnabled(false);
+                    rePassInput.setErrorEnabled(false);
+
                     //Change credentials
                     try {
                         manager.changeCredentials(mNewUserTextView.getText().toString(), mNewPassTextView.getText().toString());
